b881e3dcc602794c2ed883973fb46627f4f842e5
kritanksharma <kritanksharma3@gmail.com>
2024-07-07 14:15:44 +0530

    Minor changes for footer

--- src/Components/FooterSection1.js
+++ src/Components/FooterSection1.js
@@ -6,7 +6,7 @@ export default function FooterSection1(props) {
   return (
     <div
       id="footer"
-      className="card w-100 bg-light p-1"
+      className="card w-100 bg-light py-1"
       style={{
         height: "100%",
         borderRadius: "0px",
